21cef77e7aabd34195ec7bbe484230c5efe96d80
SEbastianSpin <sebasespinalejo@gmial.com>
2022-04-17 18:24:52 +0200

    url getten from comand line Argv

--- quoteScraper.js
+++ quoteScraper.js
@@ -1,45 +1,26 @@
 const rp = require('request-promise');
-const url = 'https://es.wikiquote.org/wiki/Sim%C3%B3n_Bol%C3%ADvar';
+
 const cheerio = require('cheerio')
 const $ = cheerio.load('<body>')
 
 
-rp(url)
-  .then(function(html){
-    //success!
-    let q=$('h2 ~ul  li', html).text();
-    console.log(q.match(/«(.*?)»/g));
-    // for (const key in q) {
-    //     let un=(q[key]);
-    //     console.log(un)
-    //     //console.log(q.match('/«(.*)»/'));
-    // }
-   
-    //console.log(filtered);
-   
-    console.log($('h2 ~ul  li', html).length);
-    //console.log($('h2 ~ul  li', html).text());
-    console.log("done");
-  })
-  .catch(function(err){
-    console.log(err);
-  });
+
+const url = process.argv[2];
+if(url){
+    rp(url)
+    .then(function(html){
+      //success!
+      let q=$('h2 ~ul  li', html).text();
+      let quotes=q.match(/«(.*?)»/g);
+      console.log(quotes);
+    })
+    .catch(function(err){
+      console.log(err);
+    });
+  
+}
+else console.log("No url given node quoteScrapper.js <<url>>");
+
 
 
   
-// rp(url)
-// .then(function(html){
-//   //success!
-//   let q=$('h2 ~ul  li', html).text();
-
-//   let filtered=[...q.match('/«(.*)»/')];
- 
-//   console.log(filtered);
-//   console.log(q);
-//   console.log($('h2 ~ul  li', html).length);
-//   //console.log($('h2 ~ul  li', html).text());
-//   console.log("done");
-// })
-// .catch(function(err){
-//   console.log(err);
-// });
